442c392caf54c1f96357cd738904ff7ec67a0983
kenny <baby831109@yahoo.com.tw>
2020-05-07 17:21:20 +0800

    Initial Commit

---
+++ KaohsiungApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KaohsiungApp
+//
+//  Created by 逸唐陳 on 2020/5/7.
+//  Copyright © 2020 逸唐陳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
