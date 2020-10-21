6e95239c0a048db8c502515d13ea58e699856875
kenny <baby831109@yahoo.com.tw>
2020-10-21 10:51:21 +0800

    test ci

--- KaohsiungApp/AppDelegate/AppDelegate.swift
+++ KaohsiungApp/AppDelegate/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     if #available(iOS 13, *) {
       //
     }else {
+      print("Test")
       let launchScreen = LaunchScreenController()
       let tabbarVC = TabBarController()
       window?.rootViewController = launchScreen
